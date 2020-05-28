d6fa44c21fea203c19b16f4362a9213e74632d8d
Alena <argada113@gmail.com>
2020-05-28 19:00:50 +0200

    discriminators and sub idx

--- python/addBTV.py
+++ python/addBTV.py
@@ -9,7 +9,21 @@ def update_jets_AK4(process):
     # Based on ``nanoAOD_addDeepInfo``
     # in https://github.com/cms-sw/cmssw/blob/master/PhysicsTools/NanoAOD/python/nano_cff.py
     _btagDiscriminators = [
-        'pfJetProbabilityBJetTags'
+        'pfJetProbabilityBJetTags',
+	'pfDeepCSVJetTags:probb',
+        'pfNegativeDeepCSVJetTags:probb',
+        'pfPositiveDeepCSVJetTags:probb',
+        'pfDeepCSVJetTags:probc',
+        'pfNegativeDeepCSVJetTags:probc',
+        'pfPositiveDeepCSVJetTags:probc',
+	'pfDeepCSVJetTags:probbb',
+        'pfNegativeDeepCSVJetTags:probbb',
+        'pfPositiveDeepCSVJetTags:probbb',
+	'pfDeepCSVJetTags:probudsg',
+	'pfNegativeDeepCSVJetTags:probudsg',
+        'pfPositiveDeepCSVJetTags:probudsg',
+	#'pfCombinedMVAV2BJetTags',
+        #'pfCombinedInclusiveSecondaryVertexV2BJetTags'
     ]
     updateJetCollection(
         process,
@@ -31,7 +45,21 @@ def update_jets_AK8(process):
     # Based on ``nanoAOD_addDeepInfoAK8``
     # in https://github.com/cms-sw/cmssw/blob/master/PhysicsTools/NanoAOD/python/nano_cff.py
     _btagDiscriminators = [
-        'pfJetProbabilityBJetTags'
+        'pfJetProbabilityBJetTags',
+	'pfDeepCSVJetTags:probb',
+        'pfNegativeDeepCSVJetTags:probb',
+        'pfPositiveDeepCSVJetTags:probb',
+        'pfDeepCSVJetTags:probc',
+        'pfNegativeDeepCSVJetTags:probc',
+        'pfPositiveDeepCSVJetTags:probc',
+        'pfDeepCSVJetTags:probbb',
+        'pfNegativeDeepCSVJetTags:probbb',
+        'pfPositiveDeepCSVJetTags:probbb',
+        'pfDeepCSVJetTags:probudsg',
+        'pfNegativeDeepCSVJetTags:probudsg',
+	'pfPositiveDeepCSVJetTags:probudsg',
+	#'pfCombinedMVAV2BJetTags',
+        #'pfCombinedInclusiveSecondaryVertexV2BJetTags'
         ]
     updateJetCollection(
         process,
@@ -57,7 +85,21 @@ def update_jets_AK8_subjet(process):
     # in https://github.com/cms-sw/cmssw/blob/master/PhysicsTools/NanoAOD/python/nano_cff.py
     # and https://github.com/alefisico/RecoBTag-PerformanceMeasurements/blob/10_2_X_boostedCommissioning/test/runBTagAnalyzer_cfg.py
     _btagDiscriminators = [
-        'pfJetProbabilityBJetTags'
+        'pfJetProbabilityBJetTags',
+	'pfDeepCSVJetTags:probb',
+        'pfNegativeDeepCSVJetTags:probb',
+        'pfPositiveDeepCSVJetTags:probb',
+        'pfDeepCSVJetTags:probc',
+        'pfNegativeDeepCSVJetTags:probc',
+        'pfPositiveDeepCSVJetTags:probc',
+        'pfDeepCSVJetTags:probbb',
+        'pfNegativeDeepCSVJetTags:probbb',
+        'pfPositiveDeepCSVJetTags:probbb',
+        'pfDeepCSVJetTags:probudsg',
+	'pfNegativeDeepCSVJetTags:probudsg',
+        'pfPositiveDeepCSVJetTags:probudsg',
+       	'pfCombinedMVAV2BJetTags',
+        #'pfCombinedInclusiveSecondaryVertexV2BJetTags'
         ]
     updateJetCollection(
         process,
@@ -79,6 +121,7 @@ def update_jets_AK8_subjet(process):
 def add_BTV(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
     addAK4 = not onlyAK8
     addAK8 = not onlyAK4
+    addMC = runOnMC
 
     if addAK4:
         process = update_jets_AK4(process)
@@ -100,7 +143,21 @@ def add_BTV(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
         nCHadrons=Var("jetFlavourInfo().getcHadrons().size()",
                       int,
                       doc="number of c-hadrons"),
-    )
+	#RENAME THEM!
+	btagDeepB_sin=Var("bDiscriminator('pfDeepCSVJetTags:probb')",
+			float,
+			doc="DeepCSV b tag discriminator",
+			precision=22),
+	btagDeepB_dob=Var("bDiscriminator('pfDeepCSVJetTags:probbb')",
+			float,
+			doc="DeepCSV bb tag discriminator",
+			precision=22),
+ 	btagDeepL = Var("bDiscriminator('pfDeepCSVJetTags:probudsg')",
+			float,
+			doc="DeepCSV light btag discriminator",
+			precision=22),
+	#RENAME THEM! If you can 
+   )
 
     # AK4
     process.customJetExtTable = cms.EDProducer(
@@ -112,7 +169,7 @@ def add_BTV(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
         singleton=cms.bool(False),  # the number of entries is variable
         extension=cms.bool(True),  # this is the extension table for Jets
         variables=cms.PSet(
-            CommonVars,
+	        CommonVars,
         ))
 
     # AK8
@@ -125,7 +182,12 @@ def add_BTV(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
         singleton=cms.bool(False),  # the number of entries is variable
         extension=cms.bool(True),  # this is the extension table for FatJets
         variables=cms.PSet(
-            CommonVars,
+        CommonVars,
+		 btagDeepC = Var("bDiscriminator('pfDeepCSVJetTags:probc')",
+                        float,
+                        doc="DeepCSV charm btag discriminator",
+                        precision=22),
+		
         ))
 
     # Subjets
@@ -143,12 +205,35 @@ def add_BTV(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
             #           float,
             #           doc="Jet Probability (Usage:BTV)",
             #           precision=10),
-        ))
+             btagDeepC = Var("bDiscriminator('pfDeepCSVJetTags:probc')",
+                        float,
+                        doc="DeepCSV charm btag discriminator",
+                        precision=22),
+
+	))
+
+    process.customSubJetMCExtTable = cms.EDProducer(
+	"SimpleCandidateFlatTableProducer",
+	src = subJetTable.src,
+	cut = subJetTable.cut,
+    	name = subJetTable.name,
+    	doc=subJetTable.doc,
+	singleton = cms.bool(False),
+   	extension = cms.bool(True),
+    	variables = cms.PSet(
+        	genJetAK8Idx = Var("?genJetFwdRef().backRef().isNonnull()?genJetFwdRef().backRef().key():-1", 
+		int, 
+		doc="index of matched gen AK8 jet"),
+   	)
+    )
 
     if addAK4:
         process.customizeJetTask.add(process.customJetExtTable)
     if addAK8:
         process.customizeJetTask.add(process.customFatJetExtTable)
         process.customizeJetTask.add(process.customSubJetExtTable)
+    if addMC and addAK8:
+	process.customizeJetTask.add(process.customSubJetMCExtTable) 
 
     return process
+
