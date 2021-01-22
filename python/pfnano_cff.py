d1d4728c563260a57dc219dd128c75854c599de8
Alejandro <alejandro.gomez@cern.ch>
2021-01-22 16:00:30 +0100

    changing JMAR to PF, adding UL files

--- python/pfnano_cff.py
+++ python/pfnano_cff.py
@@ -1,65 +1,65 @@
 import FWCore.ParameterSet.Config as cms
-from PhysicsTools.NanoAODJMAR.addPFCands_cff import addPFCands
-from PhysicsTools.NanoAODJMAR.addBTV import add_BTV
+from PhysicsTools.PFNano.addPFCands_cff import addPFCands
+from PhysicsTools.PFNano.addBTV import add_BTV
 from PhysicsTools.NanoAOD.common_cff import Var
 
 
-def JMARnano_customizeMC(process):
+def PFnano_customizeMC(process):
     addPFCands(process, True)
     add_BTV(process, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeMC_allPF(process):
+def PFnano_customizeMC_allPF(process):
     addPFCands(process, True, True)
     add_BTV(process, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
 
-def JMARnano_customizeMC_AK4JetsOnly(process):
+def PFnano_customizeMC_AK4JetsOnly(process):
     addPFCands(process, True, False, True)
     add_BTV(process, True, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeMC_AK8JetsOnly(process):
+def PFnano_customizeMC_AK8JetsOnly(process):
     addPFCands(process, True, False, False, True)
     add_BTV(process, True, False, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeMC_noInputs(process):
+def PFnano_customizeMC_noInputs(process):
     add_BTV(process, True, keepInputs=False)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
 #### DATA customization
-def JMARnano_customizeData(process):
+def PFnano_customizeData(process):
     addPFCands(process, False)
     add_BTV(process, False)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeData_allPF(process):
+def PFnano_customizeData_allPF(process):
     addPFCands(process, False, True)
     add_BTV(process, False)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeData_AK4JetsOnly(process):
+def PFnano_customizeData_AK4JetsOnly(process):
     addPFCands(process, False, False, True)
     add_BTV(process, False, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeData_AK8JetsOnly(process):
+def PFnano_customizeData_AK8JetsOnly(process):
     addPFCands(process, False, False, False, True)
     add_BTV(process, False, False, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeData_noInputs(process):
+def PFnano_customizeData_noInputs(process):
     add_BTV(process, False, keepInputs=False)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
--- test/nano106Xv8_on_mini106X_2016postVFP_NANO.py
+++ test/nano106Xv8_on_mini106X_2016postVFP_NANO.py
@@ -2,13 +2,13 @@
 # using:
 # Revision: 1.19
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
-# with command line options: nano_mc_2017_preUL --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --step NANO --conditions 102X_mc2017_realistic_v8 --era Run2_2017,run2_nanoAOD_94XMiniAODv2 --customise_commands=process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False))) --nThreads 4 -n 1000 --filein /store/mc/RunIIFall17MiniAODv2/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/270000/B4131826-50C9-E811-852D-001E675811CC.root --fileout file:nano_mc2017.root --nThreads 1 --customise PhysicsTools/PFNano/nano_jmar_cff.PFnano_customizeMC_noInputs --no_exec
+# with command line options: nano106Xv8_on_mini106X_2016postVFP --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --step NANO --conditions 106X_mcRun2_asymptotic_v15 --era Run2_2016,run2_nanoAOD_106Xv1 --customise_commands=process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False))) --nThreads 4 -n 100 --filein /store/mc/RunIISummer19UL16MiniAOD/QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8/MINIAODSIM/106X_mcRun2_asymptotic_v13-v2/100000/EF482706-F402-E648-AB6A-D5B2700C8023.root --customise PhysicsTools/PFNano/pfnano_cff.PFnano_customizeMC
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Era_Run2_2017_cff import Run2_2017
-from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv2_cff import run2_nanoAOD_94XMiniAODv2
+from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
+from Configuration.Eras.Modifier_run2_nanoAOD_106Xv1_cff import run2_nanoAOD_106Xv1
 
-process = cms.Process('NANO',Run2_2017,run2_nanoAOD_94XMiniAODv2)
+process = cms.Process('NANO',Run2_2016,run2_nanoAOD_106Xv1)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -23,12 +23,12 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
 
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/mc/RunIIFall17MiniAODv2/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/270000/B4131826-50C9-E811-852D-001E675811CC.root'),
+    fileNames = cms.untracked.vstring('/store/mc/RunIISummer19UL16MiniAOD/QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8/MINIAODSIM/106X_mcRun2_asymptotic_v13-v2/100000/EF482706-F402-E648-AB6A-D5B2700C8023.root'),
     secondaryFileNames = cms.untracked.vstring()
 )
 
@@ -38,7 +38,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('nano_mc_2017_preUL nevts:1000'),
+    annotation = cms.untracked.string('nano106Xv8_on_mini106X_2016postVFP nevts:100'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -52,7 +52,7 @@ process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
         dataTier = cms.untracked.string('NANOAODSIM'),
         filterName = cms.untracked.string('')
     ),
-    fileName = cms.untracked.string('file:nano_mc2017.root'),
+    fileName = cms.untracked.string('nano106Xv8_on_mini106X_2016postVFP_NANO.root'),
     outputCommands = process.NANOAODSIMEventContent.outputCommands
 )
 
@@ -60,7 +60,7 @@ process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
 
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '102X_mc2017_realistic_v8', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_mcRun2_asymptotic_v15', '')
 
 # Path and EndPath definitions
 process.nanoAOD_step = cms.Path(process.nanoSequenceMC)
@@ -72,6 +72,11 @@ process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step,process
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(4)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+
 # customisation of the process.
 
 # Automatic addition of the customisation function from PhysicsTools.NanoAOD.nano_cff
@@ -80,11 +85,15 @@ from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeMC
 #call to customisation function nanoAOD_customizeMC imported from PhysicsTools.NanoAOD.nano_cff
 process = nanoAOD_customizeMC(process)
 
-# Automatic addition of the customisation function from PhysicsTools.PFNano.nano_jmar_cff
-from PhysicsTools.PFNano.pfnano_cff import PFnano_customizeMC_noInputs
+# Automatic addition of the customisation function from PhysicsTools.PFNano.pfnano_cff
+from PhysicsTools.PFNano.pfnano_cff import PFnano_customizeMC, PFnano_customizeMC_allPF, PFnano_customizeMC_AK4JetsOnly, PFnano_customizeMC_AK8JetsOnly, PFnano_customizeMC_noInputs
 
-#call to customisation function PFnano_customizeMC_noInputs imported from PhysicsTools.PFNano.nano_jmar_cff
-process = PFnano_customizeMC_noInputs(process)
+#call to customisation function PFnano_customizeMC imported from PhysicsTools.PFNano.pfnano_cff
+process = PFnano_customizeMC(process)
+#process = PFnano_customizeMC_allPF(process)
+#process = PFnano_customizeMC_AK4JetsOnly(process)
+#process = PFnano_customizeMC_AK8JetsOnly(process)
+#process = PFnano_customizeMC_noInputs(process)
 
 # End of customisation functions
 
