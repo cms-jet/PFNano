e529fdbf183814deceec31ddcb8943a673ae0a30
David <david.renhwa.yu@gmail.com>
2021-10-21 11:02:09 -0500

    Restore production_tag.py and update for MiniAODv2

---
+++ production/production_tag.py
@@ -0,0 +1,6 @@
+# NOTE: Please don't delete any lines, but rather comment out and add a new line at the bottom. 
+#skim_version = "test1" # First working AK15
+#production_tag = "v1_0" # Launching first production
+#production_tag = "v1_0_1" # Debugging rare crash related to InvalidPointer, GenJetCand not found
+#production_tag = "v1_1" # Fixed the crash in v1_0_1
+production_tag = "v2_0" # Migrating to MiniAODv2 / NanoAODv9, for the new PUPPI tune and JECs
