259d5efc442d884ebd63b45f197a9903fa771767
Andrzej <novak5andrzej@gmail.com>
2024-03-18 16:24:42 +0100

    full sample lists

--- production/submit.py
+++ production/submit.py
@@ -20,8 +20,8 @@ from production_tag import production_tag # Get from a text file
 # Get from git tag (tbd)
 #production_tag = "vTEST9" # Specify by hand
 requestname_base = "pfnano"
-output_site = "T3_US_FNALLPC"
-output_lfn_base = "/store/group/lpcpfnano/{username}/{production_tag}".format(
+output_site = "T2_US_MIT"
+output_lfn_base = "/store/user/{username}/pfnano/{production_tag}".format(
                                                     username=getUsername(), 
                                                     production_tag=production_tag)
 
